9a2f34d42d80b1db91c16429d24d3c9894b3859b
Chubbard022 <chubbard022@gmail.com>
2019-04-02 12:36:29 -0700

    refactoring complete

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,38 +1,36 @@
-// import React, { Component } from 'react';
-// import axios from 'axios';
-// import MovieCard from './MovieCard';
+import React, { Component } from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard';
 
 
-// export default class Movie extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       movie: null
-//     };
-//   }
+export default class Movie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: null
+    };
+  }
 
-//   componentDidMount() {
-//     const id = this.props.match.params.id;
-//     this.fetchMovie(id);
-//   }
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.fetchMovie(id);
+  }
 
-//   fetchMovie = id => {
-//     console.log(axios)
-//     axios
-//       .get(`http://localhost:5000/api/movies/${id}`)
-//       .then(response => {
-//         this.setState(() => ({ movie: response.data }));
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   };
-//   render() {
-//     if (!this.state.movie) {
-//       return <div>Loading movie information...</div>;
-//     }
-//     {this.state.movies.map(movie => (
-//         <MovieCard key={movie.id} movie={movie} />
-//     ))}
-//   }
-// }
+  fetchMovie = id => {
+    console.log(axios)
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        this.setState(() => ({ movie: response.data }));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+  render() {
+    if (!this.state.movie) {
+      return <div>Loading movie information...</div>;
+    }
+    return(<MovieCard movie={this.state.movie} />)
+  }
+}
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,37 +1,37 @@
-// import React, { Component } from 'react';
-// import MovieCard from './MovieCard';
-// import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 
-// export default class MovieList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       movies: []
-//     };
-//   }
+export default class MovieList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: []
+    };
+  }
 
-//   componentDidMount() {
-//     axios
-//       .get('http://localhost:5000/api/movies')
-//       .then(response => {
-//         this.setState(() => ({ movies: response.data }));
-//       })
-//       .catch(error => {
-//         console.error('Server Error', error);
-//       });
-//   }
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(response => {
+        this.setState(() => ({ movies: response.data }));
+      })
+      .catch(error => {
+        console.error('Server Error', error);
+      });
+  }
 
-//   render() {
-//     return (
-//       <div className="movie-list">
-//         {this.state.movies.map(movie => (
-//           <Link key={movie.id} to={`/movies/${movie.id}`}>
-//             <MovieCard key={movie.id} movie={movie} />
-//           </Link>
-//         ))}
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div className="movie-list">
+        {this.state.movies.map(movie => (
+          <Link key={movie.id} to={`/movies/${movie.id}`}>
+            <MovieCard key={movie.id} movie={movie} />
+          </Link>
+        ))}
+      </div>
+    );
+  }
+}
